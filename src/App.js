dfa589f93b896b95f8da3d5aa1fbe156c1cc1815
ZhengJieGan <ganzj45@gmail.com>
2022-07-22 04:21:25 +0800

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Grid, Typography, Box } from "@mui/material";
+import { ReactComponent as Background } from "./assets/money.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      width="100%"
+      sx={{
+        background: "linear-gradient(to right bottom, #11998e, #38ef7d)",
+      }}
+    >
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        xs={10}
+        sm={10}
+        md={10}
+        lg={6}
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          textAlign="flex-start"
+          alignItems="flex-start"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Typography variant="h2" fontWeight="bold" color="white" gutterBottom>
+            Welcome to Broke!
+          </Typography>
+          <Typography
+            variant="h5"
+            fontWeight="medium"
+            color="white"
+            gutterBottom
+          >
+            A super user friendly platform to records all of your spendings.
+          </Typography>
+          <Typography variant="h5" color="white">
+            Helps you not to be so broke
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        xs={10}
+        sm={10}
+        md={10}
+        lg={6}
+      >
+        <Box height={{"0%"}} width={{"0%"}}>
+          <Background />
+        </Box>
+      </Grid>
+    </Grid>
   );
 }
 
